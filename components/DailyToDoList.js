55b543a5f4e6bc2dcb45c597fd2f2c5251d854e4
Ole <oleh.kodehode@gmail.com>
2023-06-29 17:11:03 +0200

    ChatGTP added delete confirmation

--- components/DailyToDoList.js
+++ components/DailyToDoList.js
@@ -2,122 +2,125 @@ import React, { useState, useEffect } from 'react';
 import styles from '../styles/DailyTodoGoals.module.css';
 
 const DailyToDoList = () => {
-  const [todos, setTodos] = useState(() => {
-    if (typeof window !== 'undefined') {
-      const storedTodos = localStorage.getItem('todos');
-      return storedTodos ? JSON.parse(storedTodos) : [];
-    }
-    return [];
-  });
-  const [newGoal, setNewGoal] = useState('');
+    const [todos, setTodos] = useState(() => {
+        if (typeof window !== 'undefined') {
+            const storedTodos = localStorage.getItem('todos');
+            return storedTodos ? JSON.parse(storedTodos) : [];
+        }
+        return [];
+    });
+    const [newGoal, setNewGoal] = useState('');
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
-  }, [todos]);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('todos', JSON.stringify(todos));
+        }
+    }, [todos]);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const currentDate = new Date();
-      const storedDate = localStorage.getItem('date');
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const currentDate = new Date();
+            const storedDate = localStorage.getItem('date');
 
-      if (!storedDate || new Date(storedDate).getDate() !== currentDate.getDate()) {
-        resetCheckboxes();
-      }
-    }
-  }, []);
+            if (!storedDate || new Date(storedDate).getDate() !== currentDate.getDate()) {
+                resetCheckboxes();
+            }
+        }
+    }, []);
 
-  const resetCheckboxes = () => {
-    const updatedTodos = todos.map((todo) => ({ ...todo, done: false }));
-    setTodos(updatedTodos);
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('date', new Date().toString());
-    }
-  };
+    const resetCheckboxes = () => {
+        const updatedTodos = todos.map((todo) => ({ ...todo, done: false }));
+        setTodos(updatedTodos);
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('date', new Date().toString());
+        }
+    };
 
-  const handleCheckboxChange = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos[index].done = !updatedTodos[index].done;
-    setTodos(updatedTodos);
-  };
+    const handleCheckboxChange = (index) => {
+        const updatedTodos = [...todos];
+        updatedTodos[index].done = !updatedTodos[index].done;
+        setTodos(updatedTodos);
+    };
 
-  const handleInputChange = (e) => {
-    setNewGoal(e.target.value);
-  };
+    const handleInputChange = (e) => {
+        setNewGoal(e.target.value);
+    };
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      handleAddGoal();
-    }
-  };
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleAddGoal();
+        }
+    };
 
-  const handleAddGoal = () => {
-    if (newGoal.trim()) {
-      const newTodo = { goal: newGoal, done: false };
-      setTodos([...todos, newTodo]);
-      setNewGoal('');
-      if (typeof window !== 'undefined') {
-        localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
-      }
-    }
-  };
+    const handleAddGoal = () => {
+        if (newGoal.trim()) {
+            const newTodo = { goal: newGoal, done: false };
+            setTodos([...todos, newTodo]);
+            setNewGoal('');
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
+            }
+        }
+    };
 
-  const handleDeleteGoal = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos.splice(index, 1);
-    setTodos(updatedTodos);
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('todos', JSON.stringify(updatedTodos));
-    }
-  };
+    const handleDeleteGoal = (index) => {
+        const confirmDelete = window.confirm('Are you sure you want to delete this goal?');
+        if (confirmDelete) {
+            const updatedTodos = [...todos];
+            updatedTodos.splice(index, 1);
+            setTodos(updatedTodos);
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('todos', JSON.stringify(updatedTodos));
+            }
+        }
+    };
 
-  return (
-    <div className={styles['todo-list']}>
-      <ul>
-  {todos.map((todo, index) => (
-    <li key={index}>
-      <label>
-        <input
-          type="checkbox"
-          checked={todo.done}
-          onChange={() => handleCheckboxChange(index)}
-        />
-        <span>{todo.goal}</span>
-      </label>
-      <div className={styles['delete-button-wrapper']}>
-        <button
-          type="button"
-          onClick={() => handleDeleteGoal(index)}
-          aria-label="Delete goal"
-          className={styles['delete-button']}
-        >
-          Delete
-        </button>
-      </div>
-    </li>
-  ))}
-</ul>
-      <div className={styles['add-button']}>
-        <input
-          type="text"
-          id="newGoal"
-          placeholder="Add a new goal..."
-          value={newGoal}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-        />
-        <button
-          type="button"
-          onClick={handleAddGoal}
-          disabled={!newGoal.trim()}
-          aria-label="Add goal"
-        >
-          Add
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles['todo-list']}>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={todo.done}
+                                onChange={() => handleCheckboxChange(index)}
+                            />
+                            <span>{todo.goal}</span>
+                        </label>
+                        <div className={styles['delete-button-wrapper']}>
+                            <button
+                                type="button"
+                                onClick={() => handleDeleteGoal(index)}
+                                aria-label="Delete goal"
+                                className={styles['delete-button']}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+            <div className={styles['add-button']}>
+                <input
+                    type="text"
+                    id="newGoal"
+                    placeholder="Add a new goal..."
+                    value={newGoal}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                />
+                <button
+                    type="button"
+                    onClick={handleAddGoal}
+                    disabled={!newGoal.trim()}
+                    aria-label="Add goal"
+                >
+                    Add
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default DailyToDoList;
